636dae144a118202742198cef9373caf1d217e0a
Vidhi Patel <vidhipatel254731@gmail.com>
2025-07-10 13:44:33 -0400

    messages

--- insight/app/components/consultant/navbar-consultant.js
+++ insight/app/components/consultant/navbar-consultant.js
@@ -101,7 +101,7 @@ export default function Header() {
                 <ClientNotificationDropdown />
               </button>
 
-              <Link href="/client/message">
+              <Link href="/consultant/message">
                 <Button variant="ghost" className="flex items-center space-x-2 relative hover:bg-blue-50 hover:text-blue-600">
                   <MessageSquare className="h-4 w-4" />
                   <span className="hidden sm:inline">Messages</span>
@@ -136,10 +136,10 @@ export default function Header() {
                 <DropdownMenuItem asChild>
                   <Link href="/account-setting">Account Settings</Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem>Document Management</DropdownMenuItem>
+              {/*     <DropdownMenuItem>Document Management</DropdownMenuItem> */}
                 <DropdownMenuItem>Subscription</DropdownMenuItem>
-                <DropdownMenuItem>Saved Clients</DropdownMenuItem>
-                <DropdownMenuItem>Preferences</DropdownMenuItem>
+             {/*   <DropdownMenuItem>Saved Clients</DropdownMenuItem>*/}
+             {/*    <DropdownMenuItem>Preferences</DropdownMenuItem>*/}
                 <DropdownMenuItem className="text-red-600">Sign Out</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
